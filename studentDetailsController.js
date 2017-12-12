a34ea512f8ff38826d4ba3d99f45a3e765429c9e
Atom Ulikhanyan <atomulikhanyan@gmail.com>
2017-12-12 23:38:20 +0400

    js code by OOP

--- studentDetailsController.js
+++ studentDetailsController.js
@@ -1,34 +1,44 @@
+function studentDetailsController(){
+	var dataCont = new dataController();
 
- 	function loadNestedInfo() {
- 		var key = 'id=';	
+//constructor
+	var editBut = document.getElementById('editBut'),
+		cancelBut = document.getElementById('cancelBut'),
+	 	saveBut = document.getElementById('saveBut'),
+	 	backBut = document.getElementById('backBut');
+
+	editBut.addEventListener("click", editButFunc, false);
+	saveBut.addEventListener('click',saveButFunc,false);
+	cancelBut.addEventListener('click',cancelButFunc,false);
+	backBut.addEventListener('click',backButFunc,false);
+
+	var id = getIdFromQuerySring();
+	var student = dataCont.getStudent(id);
+	
+	if (student == null) {
+		student = new Student();
+	}
+
+//private functions
+
+	function getIdFromQuerySring(){
+		var key = 'id=';	
  		var qsIndex = window.location.search.indexOf(key); 
 		var qsSub = window.location.search.substring(qsIndex);
 		var amp = qsSub.indexOf('&');
-
-		amp= amp == -1? qsSub.length: amp;
-
+		amp = amp == -1?qsSub.length:amp; 
 		var id = qsSub.substring(key.length,amp);
-		var student = getStudent(id);
+		return id;
+	}
 
-
-		drawStudentInfo("First Name", student.firstName);
-		drawStudentInfo("Last Name", student.lastName);
-		drawStudentInfo("Gender", student.gender);
-		drawStudentInfo("Age", student.age);
-		drawStudentInfo("Course", student.course);
-		drawStudentInfo("Department", student.department);
-		drawStudentInfo("Average Mark", student.averageMark);
-		drawStudentInfo("Phone Number", student.phoneNumber);
-		drawStudentInfo("Email", student.email);
-		
-	 }
 	 
 	function drawStudentInfo(key,value){
-		var detailsTable = document.getElementById('detailsTable'),
-			tr = document.createElement('tr'),
-			tdKey = document.createElement('td'),
-		 	tdValue = document.createElement('td'),
-		 	tdInputField = document.createElement('input');
+			var detailsTable = document.getElementById('detailsTable');
+			var d = document,
+			tr = d.createElement('tr'),
+			tdKey = d.createElement('td'),
+		 	tdValue = d.createElement('td'),
+		 	tdInputField = d.createElement('input');
 		 	
  			tdKey.innerHTML = key;
 		
@@ -41,44 +51,80 @@
 		 	tr.appendChild(tdValue);
 		 	tdValue.appendChild(tdInputField);
 
-		 	detailsTable.appendChild(tr);
+		 	detailsTable.appendChild(tr);	
 
 	}
 
-
-function setReadOnly(isReadOnly){
-	var selector = document.querySelectorAll("input[type='text']");
-	for (var i = 0; i < selector.length;i++) {
-		selector[i].readOnly = isReadOnly;
-	}
-}
-
-
-
-var editBut = document.getElementById('editBut'),
-	cancelBut = document.getElementById('cancelBut')
- 	saveBut = document.getElementById('saveBut');
-
-function editButFunc() {
-			setReadOnly(false);
- 			editBut.style.visibility = "hidden";
- 			cancelBut.style.visibility = "visible";
- 			saveBut.style.visibility = "visible";	
+	function setReadOnly(isReadOnly){													
+		var selector = document.querySelectorAll("input[type='text']");
+		for (var i = 0; i < selector.length;i++) {
+			selector[i].readOnly = isReadOnly;
+			selector[i].style.backgroundColor = "#f2f2f2";
 		}
+	}
 
-function saveButFunc(){
+
+	function editButFunc() {															//edit student's information
+		setReadOnly(false);
+ 		editBut.style.visibility = "hidden";
+ 		cancelBut.style.visibility = "visible";
+ 		saveBut.style.visibility = "visible";	
+	}
+
+	function saveButFunc(){																//save information in new version
 		setReadOnly(true);
 
+		var queryString = "";
+
+		if (!id) {
+			student.id = studList.length + 1;
+			studList.push(student);
+			queryString = "?id=" + student.id;
+		}
+
+		student.firstName = document.querySelector("input[name='First Name']").value;
+		student.lastName = document.querySelector("input[name='Last Name']").value;
+		student.age =document.querySelector("input[name='Age']").value;
+		student.gender = document.querySelector("input[name='Gender']").value;
+		student.course = document.querySelector("input[name='Course']").value;
+		student.department = document.querySelector("input[name='Department']").value;
+		student.address = document.querySelector("input[name='Address']").value;
+		student.email = document.querySelector("input[name='Email']").value;
+		student.averageMark = document.querySelector("input[name='Average Mark']").value;
+		student.phoneNumber = document.querySelector("input[name='Phone Number']").value;
+
+
+		dataCont.saveStudent(student);
+		window.location = window.location + queryString; 
 			
+	}
+
+	function cancelButFunc(){							 
+		window.location = window.location;         			//refresh page
+	}
+
+	function backButFunc(){
+		window.location = "students.html";
+	}
+
+//public functions
+
+	this.loadUserDetailsInfo=function(){
+		drawStudentInfo("First Name", student.firstName);
+		console.log(student.firstName);
+		drawStudentInfo("Last Name", student.lastName);
+		drawStudentInfo("Gender", student.gender);
+		drawStudentInfo("Age", student.age);
+		drawStudentInfo("Course", student.course);
+		drawStudentInfo("Address",student.address);
+		drawStudentInfo("Department", student.department);
+		drawStudentInfo("Average Mark", student.averageMark);
+		drawStudentInfo("Phone Number", student.phoneNumber);
+		drawStudentInfo("Email", student.email);
+
+		if (!id) {
+			editBut.click();
 		}
-
-function cancelButFunc(){
-		window.location = window.location;        //refresh page	
-		}
-
-editBut.addEventListener("click", editButFunc, false);
-saveBut.addEventListener('click',saveButFunc,false);
-cancelBut.addEventListener('click',cancelButFunc,false);
-	
-
-	
+		
+	 }
+}	
--- studentsController.js
+++ studentsController.js
@@ -1,92 +1,55 @@
+function studentsController(){
+	var dataCont = new dataController();
+	var createBut = document.getElementById('createBut');
+	var deleteBut = document.getElementById('deleteBut');
 
-function loadStudents(){
-	var students = getAllStudents();
-	for(var i=0;i<students.length;i++){
-		drawStudent(students[i]);
+	createBut.addEventListener("click", createStudent, false);
+	deleteBut.addEventListener("click", deleteList, false);
+
+	//public function
+
+	this.loadStudents = function(){
+		var students = dataCont.getAllStudents();
+		for(var i=0;i<students.length;i++){
+			drawStudent(students[i]);
+		}
+
+		function drawStudent(st){
+			var d = document;
+			var table = d.getElementById("studentsTable");
+			var tr = d.createElement("tr");
+			var tdName = d.createElement("td");
+			var tdDepartment = d.createElement("td");
+			var tdCourse = d.createElement("td");
+			var anchor = d.createElement('a');
+
+			anchor.setAttribute('href','studentdetails.html?id=' + st.id);
+			anchor.setAttribute('target','_blank');
+
+			anchor.innerHTML = st.firstName + ' ' + st.lastName;
+
+			tdName.appendChild(anchor);
+			tdDepartment.innerHTML = st.department;
+			tdCourse.innerHTML = st.course;
+
+			tr.appendChild(tdName);
+			tr.appendChild(tdDepartment);
+			tr.appendChild(tdCourse);
+
+			table.appendChild(tr);
+		
+		}
 	}
 
-	function drawStudent(st){
-		var d = document;
-		var table = d.getElementById("studentsTable");
-		var tr = d.createElement("tr");
-		var tdName = d.createElement("td");
-		var tdDepartment = d.createElement("td");
-		var tdCourse = d.createElement("td");
-		var anchor = d.createElement('a');
-
-		anchor.setAttribute('href','studentdetails.html?id=' + st.id);
-		anchor.setAttribute('target','_blank');
-
-		anchor.innerHTML = st.firstName + ' ' + st.lastName;
-
-		tdName.appendChild(anchor);
-		tdDepartment.innerHTML = st.department;
-		tdCourse.innerHTML = st.course;
-
-		tr.appendChild(tdName);
-		tr.appendChild(tdDepartment);
-		tr.appendChild(tdCourse);
-
-		table.appendChild(tr);
-		
+	//private function
+	
+	function deleteList(){
+		window.localStorage.clear();
+		window.location = window.location;
+	}
+	
+	function createStudent(){
+		window.location = "studentdetails.html?";
 	}
 
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-	// function loadNewStudents() {
-	// 	drawNewStudent('firstName');
-		
-	// }
-
-	// var createBut = document.getElementById('createBut');
-	// var newd = document.getElementById('newd');
-
-	// function drawNewStudent(key){
-
-	// 	window.location = "studentdetails.html?id=6";
-
-	// 	var d = document,
-	// 		tr = d.createElement('tr'),
-	// 		tdKey = d.createElement('td'),
-	// 	 	tdValue = d.createElement('td'),
-	// 	 	tdInputField = d.createElement('input'),
-	// 	 	editBut = d.getElementById('editBut'),
-	// 		saveBut = d.getElementById('saveBut'),
-	// 		cancelBut = d.getElementById('cancelBut'),
- 	// 		cancelBut = d.getElementById('cancelBut'),
- 	// 		saveBut = d.getElementById('saveBut');
-
- 	// 		tdInputField.setAttribute('type','text');
-	// 	 	tdInputField.readOnly = true;
-	// 	 	tdKey.innerHTML = key;
-	 
-	// 	 	tdInputField.value = '';	
-		 	
-	// 	 	tr.appendChild(tdKey);
-	// 	 	tr.appendChild(tdValue);
-	// 	 	tdValue.appendChild(tdInputField);
-
-	// 	 	newd.appendChild(tr);
-
-	// }
-
-	// createBut.addEventListener('click',loadNewStudents,false);
-
-
-
